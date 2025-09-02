3153afdf9993e26424ce19e1a614f4e91dcf8229
Matej <hozlarmatej0@gmail.com>
2025-09-02 13:26:42 +0200

    mac support

--- desktop/forge.config.cjs
+++ desktop/forge.config.cjs
@@ -3,18 +3,18 @@ const path = require("path");
 module.exports = {
   packagerConfig: {
     asar: false,
-    icon: path.resolve(__dirname, "assets", "icon.ico"),
+    icon: path.resolve(__dirname, "assets", "icon"),
+    win32metadata: {
+      CompanyName: "Matej Hozlar",
+      FileDescription: "Crypto Auto Docs",
+      ProductName: "Crypto Auto Docs",
+    },
     ignore: [
       /(^|[\\/])\.env$/i,
       /[\\/]scripts[\\/]\.env$/i,
       /[\\/]appdata[\\/]\.env$/i,
       /\.env\.[^\\/]+$/i,
     ],
-    win32metadata: {
-      CompanyName: "Matej Hozlar",
-      FileDescription: "Crypto Auto Docs",
-      ProductName: "Crypto Auto Docs",
-    },
   },
   makers: [
     {
@@ -23,6 +23,19 @@ module.exports = {
         name: "cryptoautodocs",
         setupIcon: path.resolve(__dirname, "assets", "icon.ico"),
       },
+    },
+    {
+      name: "@electron-forge/maker-dmg",
+      platforms: ["darwin"],
+      config: {
+        name: "Crypto Auto Docs",
+        title: "Crypto Auto Docs",
+        icon: path.resolve(__dirname, "assets", "icon.icns"),
+      },
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     { name: "@electron-forge/maker-zip", platforms: ["win32"] },
   ],
